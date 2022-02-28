7cb986f1aea821dbfbf35cc5a45956612a4d57e9
Ana Paula Buchholz <ana.buchholz@mrv.com.br>
2022-02-28 15:58:49 -0300

    Criando sorteador de bingo

---
+++ Bingo/Numeros.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bingo
+{
+    public class Numeros
+    {
+        List<int> listaDeNumerosASeremSorteados = new List<int>();
+        List<int> listaDeNumerosJaSorteados = new List<int>();
+
+        public Numeros()
+        {
+            for (var i = 1; i < 101; i++)
+            { 
+                listaDeNumerosASeremSorteados.Add(i);
+            }
+        }
+
+        public List<int> Sortear()
+        { 
+            var random = new Random();
+            var numeroSorteado = random.Next(1, 100);
+
+            listaDeNumerosASeremSorteados.Remove(numeroSorteado);
+
+            listaDeNumerosJaSorteados.Add(numeroSorteado);
+
+            var numeroExiste = Pesquisar(listaDeNumerosJaSorteados, numeroSorteado);
+            //listaDeNumerosJaSorteados.Any(n => n == numeroSorteado);
+
+            return listaDeNumerosJaSorteados;
+        }
+
+        public bool Pesquisar(List<int> lista, int numero) 
+        {
+            foreach (var n in lista)
+            {
+                if (n == numero)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
